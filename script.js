f2272cee2f8530fac14f08372a760ea0d9eee746
Aman Pandey <pandey.aman111@gmail.com>
2021-07-27 02:43:19 +0530

    feat: added translation from ENG to Morse Code

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+const btnReset = document.querySelector(".btn-reset");
+const btnSampleText = document.querySelector(".btn-sampleText");
+const btnTranslate = document.querySelector(".btn-translate");
+const outputText = document.querySelector("#outputText");
+const inputText = document.querySelector("#inputText");
+
+const errorHandler = () =>{
+    alert('Hi ! The API being used is public and ratelimited and can be called only 5 times per hour, and 60 times a day. Please try again after sometime.');
+}
+
+const fetchData = ()=>{
+    var input = inputText.value;
+    var URL = 'https://api.funtranslations.com/translate/morse.json?text='+input;
+    fetch(URL)
+        .then(response => response.json())
+        .then(json => outputText.value = json.contents.translated)
+        .catch(errorHandler);
+}
+
+const addSampleText = () =>{
+    inputText.value = "This is a confidential message. Don't let anyone know about this.";
+}
+
+const resetText = () =>{
+    inputText.value = "";
+}
+
+btnSampleText.addEventListener('click',addSampleText);
+btnTranslate.addEventListener('click',fetchData);
+btnReset.addEventListener('click',resetText);
